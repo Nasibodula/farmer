4f6f084aa7f166e5425ba98313ebb41ad74aab79
Nasibodula <nasiochudo@gmail.com>
2024-02-19 16:02:48 +0300

    changes on homepage

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Footer.css'
+import Facebook from '../Icons/Facebook.png'
+import Twitter from '../Icons/Twitter.png'
+import Linkedin from '../Icons/Linkedin.png'
+
+
+
+function Footer(){
+    return(
+        <div className='footer'>
+            <div className='footer-container'>
+            <div className='contact'>
+                <span value='email'>nasibochudo@gmail.com</span>
+                <span value='number'> +254741029645| +254110005220</span>
+            </div>
+            <div className='image'>
+            <img src={Facebook} alt='facebook icon'/>
+            <img src={Linkedin} alt='Linkedin icon'/>
+            <img src={Twitter} alt='Twitter icon'/>
+            </div>
+            <div className='policy'>
+                <a href='#'>Privacy Policy</a>
+                <a href='#'>Legal Services</a>
+                <a href='#'>Cookies Policy</a>
+            </div>
+            </div>
+        </div>
+    )
+}
+export default Footer;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import '../App.css'
+import About from '../components/About';
+import Footer from '../components/Footer';
 
 const Homepage = () => {
     return(
         <div>
             <Navbar/>
             <Hero/>
+            <About/>
+            <Footer/>
         </div>
     )
 }
